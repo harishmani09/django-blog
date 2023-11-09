f46a510b21064ee3c7e250f07595d58266e44805
harishmani <harishmani@gmail.com>
2023-11-09 11:57:48 +0530

    blog model and admin sections

---
+++ blogs/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Blog
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title','author','is_featured','featured_image','status','updated_at')
+    search_fields = ('id','title','author__username','status','category__category_name')
+    list_editable = ('is_featured',)
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
---
+++ blogs/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.category_name
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+STATUS_CHOICES=(
+    ('draft',"Draft"),
+    ('published',"Published"),
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True,blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    blog_body = models.TextField(max_length=2000)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft') 
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        pass
+
+    
